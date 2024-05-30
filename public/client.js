bc294c8847aa9b3a1551afff624b87ab9c8ab868
Jackie Liu <jackie@schoolhouse.world>
2024-05-29 23:19:12 -0400

    testing refresh

--- public/client.js
+++ public/client.js
@@ -1,3 +1,23 @@
+// Reset after 10 minutes
+let resetTime = 15;
+
+function setIdle(cb, seconds) {
+  var timer;
+  var interval = seconds * 1000;
+  function refresh() {
+    clearInterval(timer);
+    timer = setTimeout(cb, interval);
+  }
+  ["keypress", "click", "mousemove"].forEach((event) =>
+    document.addEventListener(event, refresh)
+  );
+  refresh();
+}
+
+setIdle(function () {
+  location.href = "/";
+}, resetTime);
+
 // uncomment the line below and put your socket events in here
 let socket = io();
 console.log(socket);
